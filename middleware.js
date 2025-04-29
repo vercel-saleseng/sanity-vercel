ecfeb600bdabd26bf39ffa3d3231aa9cf19fc0d5
Derek Wu <derekwu35@gmail.com>
2025-04-29 10:16:59 -0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,30 +1,30 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 // import { unstable_precompute as precompute } from '@vercel/flags/next';
 // import { precomputeFlags } from '@/flags';
 
-// // Note that we're running this middleware for / only, but
-// // you could extend it to further pages you're experimenting on
-// export const config = { matcher: ['/'] };
+// Note that we're running this middleware for / only, but
+// you could extend it to further pages you're experimenting on
+export const config = { matcher: ['/'] };
 
-// export async function middleware(request) {
-//     const response = NextResponse.next()
+export async function middleware(request) {
+    const response = NextResponse.next()
 
-//     // sets headers for location
-//     const city = request.geo?.city ?? 'Unknown'
-//     const latitude = request.geo?.latitude ?? 'Unknown'
-//     const longitude = request.geo?.longitude ?? 'Unknown'
+    // // sets headers for location
+    // const city = request.geo?.city ?? 'Unknown'
+    // const latitude = request.geo?.latitude ?? 'Unknown'
+    // const longitude = request.geo?.longitude ?? 'Unknown'
 
-//     // add the location information to the request headers
-//     response.headers.set('x-user-city', city)
-//     response.headers.set('x-user-latitude', latitude.toString())
-//     response.headers.set('x-user-longitude', longitude.toString())
+    // // add the location information to the request headers
+    // response.headers.set('x-user-city', city)
+    // response.headers.set('x-user-latitude', latitude.toString())
+    // response.headers.set('x-user-longitude', longitude.toString())
 
-//     // sets headers for the flag
-//     // precompute returns a string encoding each flag's returned value
-//     // https://vercel.com/docs/workflow-collaboration/feature-flags/flags-pattern-nextjs#precomputing-flags
-//     const code = await precompute(precomputeFlags);
+    // // sets headers for the flag
+    // // precompute returns a string encoding each flag's returned value
+    // // https://vercel.com/docs/workflow-collaboration/feature-flags/flags-pattern-nextjs#precomputing-flags
+    // const code = await precompute(precomputeFlags);
 
-//     response.headers.set('encoded-feature-flags', code);
+    // response.headers.set('encoded-feature-flags', code);
 
-//     return response;
-// }
+    return response;
+}
