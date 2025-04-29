a4056af7985d9bde13d2a10908fd33e200208e2e
Derek Wu <derekwu35@gmail.com>
2025-04-29 09:58:18 -0700

    update to sanity app

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -9,23 +9,17 @@ import {
 } from "next-sanity";
 import { Inter } from "next/font/google";
 import { draftMode } from "next/headers";
-import { Suspense } from "react";
+
 import AlertBanner from "./alert-banner";
 import PortableText from "./portable-text";
 
-import type { SettingsQueryResult } from "@/sanity.types";
 import * as demo from "@/sanity/lib/demo";
 import { sanityFetch } from "@/sanity/lib/fetch";
 import { settingsQuery } from "@/sanity/lib/queries";
 import { resolveOpenGraphImage } from "@/sanity/lib/utils";
-import { showAd } from "@/flags";
-import BannerAd from "./banner-ad";
-import { FlagValues } from '@vercel/flags/react';
-import { headers } from 'next/headers'
-
 
 export async function generateMetadata(): Promise<Metadata> {
-  const settings = await sanityFetch<SettingsQueryResult>({
+  const settings = await sanityFetch({
     query: settingsQuery,
     // Metadata should never contain stega
     stega: false,
@@ -61,75 +55,55 @@ const inter = Inter({
   display: "swap",
 });
 
-async function Footer() {
-  const data = await sanityFetch<SettingsQueryResult>({
-    query: settingsQuery,
-  });
-  const footer = data?.footer || [];
-
-  return (
-    <footer className="bg-accent-1 border-accent-2 border-t">
-      <div className="container mx-auto px-5">
-        {footer.length > 0 ? (
-          <PortableText
-            className="prose-sm text-pretty bottom-0 w-full max-w-none bg-white py-12 text-center md:py-20"
-            value={footer as PortableTextBlock[]}
-          />
-        ) : (
-          <div className="flex flex-col items-center py-28 lg:flex-row">
-            <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-              Built with Next.js.
-            </h3>
-            <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-              <a
-                href="https://nextjs.org/docs"
-                className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
-              >
-                Read Documentation
-              </a>
-              <a
-                href="https://github.com/vercel/next.js/tree/canary/examples/cms-sanity"
-                className="mx-3 font-bold hover:underline"
-              >
-                View on GitHub
-              </a>
-            </div>
-          </div>
-        )}
-      </div>
-    </footer>
-  );
-}
-
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-  const headersList = headers()
-  const city = headersList.get('x-user-city') || 'Error'
-  const ad = await showAd()
+  const data = await sanityFetch({ query: settingsQuery });
+  const footer = data?.footer || [];
+  const { isEnabled: isDraftMode } = await draftMode();
 
   return (
     <html lang="en" className={`${inter.variable} bg-white text-black`}>
       <body>
-        <FlagValues values={{ [showAd.key]: ad }} />
-
         <section className="min-h-screen">
-          {draftMode().isEnabled && <AlertBanner />}
-          <Suspense>
-            {ad ? <BannerAd cityName={city} /> : null}
-          </Suspense>
+          {isDraftMode && <AlertBanner />}
           <main>{children}</main>
-          <Suspense>
-            <Footer />
-          </Suspense>
+          <footer className="bg-accent-1 border-accent-2 border-t">
+            <div className="container mx-auto px-5">
+              {footer.length > 0 ? (
+                <PortableText
+                  className="prose-sm text-pretty bottom-0 w-full max-w-none bg-white py-12 text-center md:py-20"
+                  value={footer as PortableTextBlock[]}
+                />
+              ) : (
+                <div className="flex flex-col items-center py-28 lg:flex-row">
+                  <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
+                    Built with Next.js.
+                  </h3>
+                  <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
+                    <a
+                      href="https://nextjs.org/docs"
+                      className="mx-3 mb-6 border border-black bg-black py-3 px-12 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black lg:mb-0 lg:px-8"
+                    >
+                      Read Documentation
+                    </a>
+                    <a
+                      href="https://github.com/vercel/next.js/tree/canary/examples/cms-sanity"
+                      className="mx-3 font-bold hover:underline"
+                    >
+                      View on GitHub
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </footer>
         </section>
-        {draftMode().isEnabled && <VisualEditing />}
+        {isDraftMode && <VisualEditing />}
         <SpeedInsights />
       </body>
     </html>
   );
 }
-
